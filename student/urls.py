e16cf55760601a6e58bdf35bb92139a3d0969c70
dodosiz2 <asvestopouli@hotmail.com>
2018-09-20 01:23:46 +0300

    Some more views and the initial form

--- student/urls.py
+++ student/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    url('^$', views.students_view, name='index')
+    url('^full_info/', views.full_info, name='full_info'),
+    url('^basic_info/', views.basic_info, name='basic_info'),
+    url('^exam_info/', views.exam_info, name='exam_info'),
+    url('^financial_info', views.financial_info, name='financial_info'),
+    url('^detail/(?P<student_id>[0-9]+)/', views.detail, name='detail')
 ]
--- student/views.py
+++ student/views.py
@@ -1,8 +1,40 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models.student import Student
+from .forms import StudentForm
 
 
-def students_view(request):
+def full_info(request):
     students = Student.objects.all()
     context = {'students': students}
-    return render(request, 'index.html', context)
+    return render(request, 'full.html', context)
+
+
+def basic_info(request):
+    students = Student.objects.all()
+    context = {'students': students}
+    return render(request, 'basic.html', context)
+
+
+def exam_info(request):
+    students = Student.objects.all()
+    context = {'students': students}
+    return render(request, 'exam.html', context)
+
+
+def financial_info(request):
+    students = Student.objects.all()
+    context = {'students': students}
+    return render(request, 'financial.html', context)
+
+
+def detail(request, student_id):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('full_info')
+    else:
+        form = StudentForm()
+
+    return render(request, 'detail.html', {'form': form})
+
